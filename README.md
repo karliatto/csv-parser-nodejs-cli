960fc1850ed2c893870d2ffeb71ed278a9c9a93b
Carlos Garcia Ortiz <karlgoso@gmail.com>
2017-04-18 20:25:24 +0200

    install commander and first program

---
+++ README.md
@@ -0,0 +1,11 @@
+# git init
+
+# npm init
+
+# hello-world.js 
+``` console.log('hello world'); ```
+
+# Install commander
+``` npm install commander --save ```
+
+# node hello-world --list 'hello world'
--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+var program = require('commander');
+
+program
+	.version('0.0.1')
+	.option('-l, --list [list]', 'list of emails in CSV file')
+	.parse(process.argv)
+
+console.log(program.list);
